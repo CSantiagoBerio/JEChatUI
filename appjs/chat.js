1236d3ef3c50e3287f7bba561843f5f6af1d4287
christian.santiago <christian.santiago23@upr.edu>
2018-05-23 23:49:54 -0400

    Implemented functionalities for like, dislike and reply delivarebales

--- appjs/chat.js
+++ appjs/chat.js
@@ -1,103 +1,198 @@
 angular.module('JEChat').controller('ChatController', ['$http', '$log', '$scope', '$routeParams',
-    function($http, $log, $scope, $routeParams) {
+function($http, $log, $scope, $routeParams) {
 
-        var thisCtrl = this;
+  var thisCtrl = this;
 
-        this.messageList = [];
-        this.reactions = {};
-        this.user = {};
-        this.counter = 0;
+  this.messageList = [];
+  this.reactions = {};
+  this.user = {};
+  this.counter = 0;
+  $scope.replyText;
 
-        this.loadMessages = function(){
+  $scope.loadMessages = function(){
 
-            var user = $routeParams.user;
-            var group = $routeParams.groupname;
+    var user = $routeParams.user;
+    var group = $routeParams.groupname;
 
-            console.log('Im inside chat.js');
-            //URL for Messages
-            var url = "https://jechat-restapi.herokuapp.com/JEChat/"+user+"/GroupChats/"+group;
-            console.log("url: " + url)
+    console.log('Im inside chat.js');
+    //URL for Messages
+    var url = "https://jechat-restapi.herokuapp.com/JEChat/"+user+"/GroupChats/"+group;
+    console.log("url: " + url)
 
-            $http.get(url).then(
-              //Success Function
-              function(response){
+    $http.get(url).then(
+      //Success Function
+      function(response){
 
-                console.log("info: " + JSON.stringify(response.data));
-                thisCtrl.messageList = response.data.Groupchats;
+        console.log("info: " + JSON.stringify(response.data));
+        thisCtrl.messageList = response.data.Groupchats;
 
-              },
-              //Error function
-              function(response){
-                var status = response.status;
-                if (status == 0) {
-                    alert("No hay conexion a Internet");
-                }
-                else if (status == 401) {
-                    alert("Su sesion expiro. Conectese de nuevo.");
-                }
-                else if (status == 403) {
-                    alert("No esta autorizado a usar el sistema.");
-                }
-                else if (status == 404) {
-                    alert("No se encontro la informacion solicitada.");
-                }
-                else {
-                    alert("Error interno del sistema.");
-                }
-              }
-            );
+      },
+      //Error function
+      function(response){
+        var status = response.status;
+        if (status == 0) {
+          alert("No hay conexion a Internet");
+        }
+        else if (status == 401) {
+          alert("Su sesion expiro. Conectese de nuevo.");
+        }
+        else if (status == 403) {
+          alert("No esta autorizado a usar el sistema.");
+        }
+        else if (status == 404) {
+          alert("No se encontro la informacion solicitada.");
+        }
+        else {
+          alert("Error interno del sistema.");
+        }
+      }
+    );
 
-        };
+  };
 
 
 
-        this.postMsg = function(){
-            var msg = thisCtrl.newText;
-            var user = $routeParams.user;
-            var nextId = thisCtrl.counter++;
-            var url = "https://jechat-restapi.herokuapp.com/JEChat/"+user+"/GroupChats/"+$routeParams.groupname;
-            var author;
+  this.postMsg = function(){
+    var msg = thisCtrl.newText;
+    var user = $routeParams.user;
+    var nextId = thisCtrl.counter++;
+    var url = "https://jechat-restapi.herokuapp.com/JEChat/"+user+"/GroupChats/"+$routeParams.groupname;
+    var form = {'content':msg}
 
-            console.log(url);
+    console.log(url);
 
-            // $http.get(url).then(
-            //   //Success Function
-            //   function(response){
-            //
-            //     console.log("info: " + JSON.stringify(response.data));
-            //     thisCtrl.user = response.data.User;
-            //     author = thisCtrl.user.firstname + " " + thisCtrl.user.lastname;
-            //
-            //     if(thisCtrl.messageList.length == 0) {nextId=0;};
-            //     console.log(nextId);
-            //     thisCtrl.messageList.unshift({"id": nextId, "content" : msg, "name" : author, "like" : 0, "nolike" : 0});
-            //     thisCtrl.newText = "";
-            //
-            //   },
-            //   //Error function
-            //   function(response){
-            //     var status = response.status;
-            //     if (status == 0) {
-            //         alert("No hay conexion a Internet");
-            //     }
-            //     else if (status == 401) {
-            //         alert("Su sesion expiro. Conectese de nuevo.");
-            //     }
-            //     else if (status == 403) {
-            //         alert("No esta autorizado a usar el sistema.");
-            //     }
-            //     else if (status == 404) {
-            //         alert("No se encontro la informacion solicitada.");
-            //     }
-            //     else {
-            //         alert("Error interno del sistema.");
-            //     }
-            //   }
-            // );
+    $http.post(url, form).then(
+      function(response){
+        console.log(JSON.stringify(response.data));
+        $scope.loadMessages();
+      },
+      function(response){
+        var status = response.status;
+        if (status == 0) {
+          alert("No hay conexion a Internet");
+        }
+        else if (status == 401) {
+          alert("Su sesion expiro. Conectese de nuevo.");
+        }
+        else if (status == 403) {
+          alert("No esta autorizado a usar el sistema.");
+        }
+        else if (status == 404) {
+          alert("No se encontro la informacion solicitada.");
+        }
+        else {
+          alert("Error interno del sistema.");
+        }
+      }
+    );
 
-            // thisCtrl.messageList.unshift({"id": nextId, "content" : msg, "name" : author, "like" : 0, "nolike" : 0});
-            // thisCtrl.newText = "";
-        };
+    this.newText = [];
 
-        this.loadMessages();
+  };
+
+  $scope.like = function(messageid){
+    var groupname = $routeParams.groupname;
+    var user = $routeParams.user;
+    var mid = messageid;
+    var url = "http://jechat-restapi.herokuapp.com/JEChat/"+groupname+"/Messages/"+mid+"/likes";
+    console.log(url +'\n  userid: '+user+'   mid: '+mid);
+    var form = {'user': user, 'mid': mid}
+
+    $http.post(url, form).then(
+      function(response){
+        console.log(response.data);
+        $scope.loadMessages();
+      },
+      function(response){
+        var status = response.status;
+        if (status == 0) {
+          alert("No hay conexion a Internet");
+        }
+        else if (status == 401) {
+          alert("Su sesion expiro. Conectese de nuevo.");
+        }
+        else if (status == 403) {
+          alert("No esta autorizado a usar el sistema.");
+        }
+        else if (status == 404) {
+          alert("No se encontro la informacion solicitada.");
+        }
+        else {
+          alert("Error interno del sistema.");
+        }
+      }
+    );
+  };
+
+  $scope.dislike = function(messageid){
+    var groupname = $routeParams.groupname;
+    var user = $routeParams.user;
+    var mid = messageid;
+    var url = "http://jechat-restapi.herokuapp.com/JEChat/"+groupname+"/Messages/"+mid+"/dislikes";
+    console.log(url +'\n  userid: '+user+'   mid: '+mid);
+    var form = {'user': user, 'mid': mid}
+
+    $http.post(url, form).then(
+      function(response){
+        console.log(response.data);
+        $scope.loadMessages();
+      },
+      function(response){
+        var status = response.status;
+        if (status == 0) {
+          alert("No hay conexion a Internet");
+        }
+        else if (status == 401) {
+          alert("Su sesion expiro. Conectese de nuevo.");
+        }
+        else if (status == 403) {
+          alert("No esta autorizado a usar el sistema.");
+        }
+        else if (status == 404) {
+          alert("No se encontro la informacion solicitada.");
+        }
+        else {
+          alert("Error interno del sistema.");
+        }
+      }
+    );
+  };
+
+  $scope.reply = function(messageid){
+    var url = "http://jechat-restapi.herokuapp.com/JEChat/"+$routeParams.groupname+"/Messages/"+messageid+"/replies";
+    console.log(url);
+    var form = {'user': $routeParams.user, 'mid': messageid, 'groupname': $routeParams.groupname, 'content': thisCtrl.replyText};
+    console.log(form);
+
+    $http.post(url, form).then(
+      function(response){
+        console.log(response.data);
+        $scope.loadMessages();
+      },
+      function(response){
+        var status = response.status;
+        if (status == 0) {
+          alert("No hay conexion a Internet");
+        }
+        else if (status == 401) {
+          alert("Su sesion expiro. Conectese de nuevo.");
+        }
+        else if (status == 403) {
+          alert("No esta autorizado a usar el sistema.");
+        }
+        else if (status == 404) {
+          alert("No se encontro la informacion solicitada.");
+        }
+        else {
+          alert("Error interno del sistema.");
+        }
+      }
+    );
+
+    thisCtrl.replyText = [];
+  };
+
+
+
+  $scope.loadMessages();
 }]);
